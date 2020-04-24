657e3cd6671402bea0866a412e5963935493aca0
Iona <ionabrabender@gmail.com>
2020-04-24 23:56:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,14 +10,14 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array,arg)
-  array.pop(arg)
+def pop_with_args(array)
+  array.pop(2)
 end
   
 def using_shift(array)
   array.shift 
 end
 
-def shift_with_args(array,arg)
-  array.shift(arg)
+def shift_with_args(array)
+  array.shift(2)
 end
